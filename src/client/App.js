003f7aeb7495b3344ff35be0f322e3bfaf44c539
Timothy Roberts <timothyroberts@Timothys-MacBook-Pro.local>
2019-04-07 12:27:18 +0100

    styled homepage

--- src/client/App.js
+++ src/client/App.js
@@ -39,35 +39,35 @@ class App extends Component {
     console.log(this.state);
     return (
       <div>
-
-      <nav className="uk-navbar-container uk-navbar" >
-          <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav">
-                  <li className="uk-active"><Link to="/">Home</Link></li>
-                  {this.state.loggedIn && <li><Link to={`/list/${this.state.user._id}`}>My Images</Link></li>}
-                  <li><Link to={`/upload/${this.state.user._id}`}>Upload</Link></li>
-                  {!this.state.loggedIn && <li><Link to="/login">Login</Link></li>}
-                  {!this.state.loggedIn && <li><Link to="/register">Register</Link></li>}
-                  {this.state.loggedIn && <li><Link to="/logout">Logout</Link></li>}
-              </ul>
+        <nav className="uk-navbar-container uk-navbar" uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+          <div className="uk-navbar-center">
+            <ul className="uk-navbar-nav">
+              <li className="uk-active uk-navbar-item uk-logo"><Link to="/">PhotoFeed</Link></li>
+              {this.state.loggedIn && <li><Link to={`/list/${this.state.user._id}`}>My Images</Link></li>}
+              <li><Link to={`/upload/${this.state.user._id}`}>Upload</Link></li>
+              {!this.state.loggedIn && <li><Link to="/login">Login</Link></li>}
+              {!this.state.loggedIn && <li><Link to="/register">Register</Link></li>}
+              {this.state.loggedIn && <li><Link to="/logout">Logout</Link></li>}
+            </ul>
           </div>
-      </nav>
-        <ul>
+        </nav>
 
-        </ul>
+        <div className="uk-container">
+          <div className="uk-padding uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true" uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 100; ">
+            <Switch>
+              // Components available to any user
+              <Route path="/" exact component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" render={(props) => <Login {...props} handleLogin={this.login} />} />
+              <Route path="/logout" render={this.logout} />
 
-        <Switch>
-          // Components available to any user
-          <Route path="/" exact component={Home} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" render={(props) => <Login {...props} handleLogin={this.login} />} />
-          <Route path="/logout" render={this.logout} />
-
-          // Components that require a log in to access
-          <Route path="/list/:id" component={withAuth(PostsList)} />
-          <Route path="/edit/:id" component={withAuth(EditImage)} />
-          <Route path="/upload/:id" component={withAuth(Upload)} />
-        </Switch>
+              // Components that require a log in to access
+              <Route path="/list/:id" component={withAuth(PostsList)} />
+              <Route path="/edit/:id" component={withAuth(EditImage)} />
+              <Route path="/upload/:id" component={withAuth(Upload)} />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
--- src/client/Home.js
+++ src/client/Home.js
@@ -4,16 +4,26 @@ import axios from "axios";
 
 
 const Image = props => (
-  <div className="uk-card uk-card-body">
-    <div>
-    <h1> {props.image.image_title} </h1>
-    <img src={props.image.image_URL} />
-    <h2> {props.image.image_description}</h2>
-
-    <Link to={"/edit/" + props.image._id}>Edit</Link>
+  <div>
+    <div className="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle">
+      <div className="uk-inline" uk-scrollspy="target: > div; cls:uk-animation-fade; delay: 500">
+        <div className="uk-text-center">
+          <div className="uk-inline-clip uk-transition-toggle" tabIndex="0">
+              <img src={props.image.image_URL} />
+              <div className="uk-transition-slide-bottom uk-position-bottom uk-overlay uk-overlay-default">
+                <h3> {props.image.image_title} </h3>
+                <p> {props.image.image_description}</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div uk-lightbox = "true">
+      <a className="uk-button uk-button-default" href={props.image.image_URL} data-caption="Caption">View Image</a>
     </div>
   </div>
 );
+
 
 export default class Home extends Component {
   constructor() {
@@ -42,7 +52,7 @@ export default class Home extends Component {
     console.log(this.props);
 
     return this.state.uploads.map(function(currentImage, i) {
-      return <Image image={currentImage} key={i} />;
+      return <div> <Image key={i} image={currentImage} /> </div>;
     });
   }
 }
